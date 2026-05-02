3877fc2a4adab338933cf817704f7a5ca0617f19
ISHIMWE FABRICE <your_email@gmail.com>
2026-05-02 13:49:32 +0200

    update video call

---
+++ server.js
@@ -0,0 +1,39 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 3000 });
+
+const rooms = {};
+
+wss.on("connection", (ws) => {
+  ws.on("message", (message) => {
+    const data = JSON.parse(message);
+
+    const room = data.room;
+
+    if (!rooms[room]) {
+      rooms[room] = [];
+    }
+
+    if (data.type === "join") {
+      rooms[room].push(ws);
+      ws.room = room;
+    }
+
+    if (data.type === "signal") {
+      rooms[room].forEach(client => {
+        if (client !== ws && client.readyState === WebSocket.OPEN) {
+          client.send(JSON.stringify(data));
+        }
+      });
+    }
+  });
+
+  ws.on("close", () => {
+    const room = ws.room;
+    if (rooms[room]) {
+      rooms[room] = rooms[room].filter(c => c !== ws);
+    }
+  });
+});
+
+console.log("WebSocket server running on ws://localhost:3000");
